65dac760cdf47514e127cbf84b0b2e40f1ccf8ab
georgedemirjian <gzdemirj@uci.edu>
2024-09-15 22:30:20 -0700

    added spinning animation

--- SLOTS PY/machine.py
+++ SLOTS PY/machine.py
@@ -7,13 +7,40 @@ class Machine:
         self.display_surface = pygame.display.get_surface()
         self.reel_list = {}
         self.reel_index = 0
+        self.can_toggle = True
+        self.spinning = False
 
         self.set_reels()
 
+    
+    def cooldowns(self):
+        for reel in self.reel_list:
+            if self.reel_list[reel].reel_is_moving:
+                self.can_toggle = False
+                self.spinning = True
+        if not self.can_toggle and [self.reel_list[reel].reel_is_moving for reel in self.reel_list].count(False) == 5:
+            self.can_toggle = True
+    
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+        #if keys[pygame.K_SPACE] and self.can_toggle and self.currentPlayer.balance >= self.currentPlayer.bet_size:
+            self.toggle_spinning()
+            self.spin_time = pygame.time.get_ticks()
+            #self.currentPlayer.place_bet()
+            #self.machine_balance += self.currentPlayer.bet_size
+            #self.currentPlayer.last_payout = None
+
     def update(self, delta_time):
+        self.get_input()
+        self.draw_reels(delta_time)
         for reel in self.reel_list:
             self.reel_list[reel].symbol_list.draw(self.display_surface)
             self.reel_list[reel].symbol_list.update()
+        
+    def draw_reels(self, delta_time):
+        for reel in self.reel_list:
+            self.reel_list[reel].animate(delta_time)
 
     def set_reels(self):
         if not self.reel_list:
@@ -22,7 +49,17 @@ class Machine:
         while self.reel_index < 5:
             if self.reel_index > 0:
                 x_tl = x_tl + 220 + X_OFFSET
-                y_tl = y_tl 
+                y_tl = y_tl
             self.reel_list[self.reel_index] = Reel((x_tl, y_tl))
             self.reel_index += 1
+    
+    def toggle_spinning(self):
+        if self.can_toggle:
+            self.spin_time = pygame.time.get_ticks()
+            self.spinning = not self.spinning
+            self.can_toggle = False
+
+            for reel in self.reel_list:
+                self.reel_list[reel].start_spin(int(reel) * 200)
+
 
--- SLOTS PY/reel.py
+++ SLOTS PY/reel.py
@@ -16,6 +16,31 @@ class Reel:
             coords = list(coords)
             coords[1] += 215
             coords = tuple(coords)
+        
+    def animate(self, delta_time):
+        if self.reel_is_moving:
+            self.delay_time -= (delta_time * 1000)
+            self.spin_time -= (delta_time *1000)
+            reel_is_stopping = False
+
+            if self.spin_time < 0:
+                reel_is_stopping = True
+            if self.delay_time <= 0:
+                for symbol in self.symbol_list:
+                    symbol.rect.bottom += 100
+
+                    if symbol.rect.top > 750:
+                        if reel_is_stopping:
+                            self.reel_is_moving = False
+                        symbol_idx = symbol.index
+                        symbol.kill()
+                        self.symbol_list.add(Symbol(IMAGES[random.choice(self.shuffled_reels)], ((symbol.x_val), -190), symbol_idx))
+
+    
+    def start_spin(self, delay_time):
+        self.delay_time = delay_time
+        self.spin_time = 1000 + delay_time
+        self.reel_is_moving = True
 
 class Symbol(pygame.sprite.Sprite):
     def __init__(self, filePath, coords, index):
